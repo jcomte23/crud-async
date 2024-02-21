ede2ff144f0307a933e68faec8de70156f6887b5
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-02-21 15:15:47 -0500

    mejora en el codigo

--- src/js/conexion_local.js
+++ src/js/conexion_local.js
@@ -2,16 +2,28 @@ import '../scss/styles.scss'
 import * as bootstrap from 'bootstrap'
 
 const URL = "http://localhost:3000/usuarios"
+const texto = document.getElementById("nombre")
 const tbody = document.getElementById("tbody")
 const Guardar = document.getElementById("Guardar")
 const Actualizar = document.getElementById("Actualizar")
-const Eliminar = document.getElementById("Eliminar")
+let idCache
 
 document.addEventListener('DOMContentLoaded', consumirDatos)
+tbody.addEventListener('click', (event) => {
+    if (event.target.classList.contains("btn-danger")) {
+        const id = event.target.getAttribute("data-id")
+        eliminarDatos(id)
+    }
+
+    if (event.target.classList.contains("btn-warning")) {
+        idCache = event.target.getAttribute("data-id")
+        actualizarDatos(idCache)
+    }
+
+})
 
 Guardar.addEventListener('click', guardarDatos)
 Actualizar.addEventListener('click', actualizarDatos)
-Eliminar.addEventListener('click', eliminarDatos)
 
 async function consumirDatos() {
     const solicitud = await fetch(URL)
@@ -24,8 +36,8 @@ async function consumirDatos() {
           <th scope="row">${element.id}</th>
           <td>${element.nombre}</td>
           <td>
-            <button id="Actualizar" type="button" class=" btn btn-warning">Editar</button>
-            <button id="Eliminar" type="button" class=" btn btn-danger">Eliminar</button>
+            <button type="button" class=" btn btn-warning" data-id="${element.id}">Editar</button>
+            <button type="button" class="btn btn-danger" data-id="${element.id}">Eliminar</button>
           </td>
         </tr>
         `
@@ -33,33 +45,63 @@ async function consumirDatos() {
 }
 
 async function guardarDatos() {
+    const usuario = {
+        nombre: texto.value
+    }
+
     const solicitud = await fetch(URL, {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({ nombre: "usuario" })
+        body: JSON.stringify(usuario)
     })
-    
+
     if (solicitud.ok) {
         alert(solicitud.statusText)
-    }else{
+    } else {
         alert(solicitud.statusText)
     }
-    
+
     consumirDatos()
 }
 
 async function actualizarDatos() {
-    const solicitud = await fetch(URL)
-    const datos = await solicitud.json()
-    console.clear()
-    console.table(datos)
+    const usuario = {
+        nombre: texto.value
+    }
+
+    const solicitud = await fetch(URL, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(usuario)
+    })
+
+    if (solicitud.ok) {
+        alert(solicitud.statusText)
+    } else {
+        alert(solicitud.statusText)
+    }
+
+    idCache = undefined
+    consumirDatos()
 }
 
-async function eliminarDatos() {
-    const solicitud = await fetch(URL)
-    const datos = await solicitud.json()
-    console.clear()
-    console.table(datos)
+async function eliminarDatos(id) {
+    const solicitud = await fetch(`${URL}/${id}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+
+    if (solicitud.ok) {
+        alert(solicitud.statusText)
+    } else {
+        alert(solicitud.statusText)
+    }
+
+    consumirDatos()
 }
