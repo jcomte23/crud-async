49d302bebf6d7ec03ea446a83288d76f48444e0f
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-02-21 14:55:31 -0500

    mejora en el crud

--- src/js/conexion_local.js
+++ src/js/conexion_local.js
@@ -1,22 +1,35 @@
 import '../scss/styles.scss'
 import * as bootstrap from 'bootstrap'
 
-const URL = "https://api.spacexdata.com/v3/launches"
-const Consumir = document.getElementById("Consumir")
+const URL = "http://localhost:3000/usuarios"
+const tbody = document.getElementById("tbody")
 const Guardar = document.getElementById("Guardar")
 const Actualizar = document.getElementById("Actualizar")
 const Eliminar = document.getElementById("Eliminar")
 
-Consumir.addEventListener('click',consumirDatos)
-Guardar.addEventListener('click',guardarDatos)
-Actualizar.addEventListener('click',actualizarDatos)
-Eliminar.addEventListener('click',eliminarDatos)
+document.addEventListener('DOMContentLoaded', consumirDatos)
+
+Guardar.addEventListener('click', guardarDatos)
+Actualizar.addEventListener('click', actualizarDatos)
+Eliminar.addEventListener('click', eliminarDatos)
 
 async function consumirDatos() {
     const solicitud = await fetch(URL)
     const datos = await solicitud.json()
-    console.clear()
-    console.table(datos)
+    tbody.innerHTML = ``
+    datos.forEach(element => {
+        tbody.innerHTML +=
+            `
+        <tr>
+          <th scope="row">${element.id}</th>
+          <td>${element.nombre}</td>
+          <td>
+            <button id="Actualizar" type="button" class=" btn btn-warning">Editar</button>
+            <button id="Eliminar" type="button" class=" btn btn-danger">Eliminar</button>
+          </td>
+        </tr>
+        `
+    })
 }
 
 async function guardarDatos() {
