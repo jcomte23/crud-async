1ebaa4b1524559a67688d54063c7a639982e342a
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-02-21 13:26:50 -0500

    creacion de las funciones asincronas

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,41 @@
 import '../scss/styles.scss'
 import * as bootstrap from 'bootstrap'
+
+const URL = "https://api.spacexdata.com/v3/launches"
+const Consumir = document.getElementById("Consumir")
+const Guardar = document.getElementById("Guardar")
+const Actualizar = document.getElementById("Actualizar")
+const Eliminar = document.getElementById("Eliminar")
+
+Consumir.addEventListener('click',consumirDatos)
+Guardar.addEventListener('click',guardarDatos)
+Actualizar.addEventListener('click',actualizarDatos)
+Eliminar.addEventListener('click',eliminarDatos)
+
+async function consumirDatos() {
+    const solicitud = await fetch(URL)
+    const datos = await solicitud.json()
+    console.clear()
+    console.table(datos)
+}
+
+async function guardarDatos() {
+    const solicitud = await fetch(URL)
+    const datos = await solicitud.json()
+    console.clear()
+    console.table(datos)
+}
+
+async function actualizarDatos() {
+    const solicitud = await fetch(URL)
+    const datos = await solicitud.json()
+    console.clear()
+    console.table(datos)
+}
+
+async function eliminarDatos() {
+    const solicitud = await fetch(URL)
+    const datos = await solicitud.json()
+    console.clear()
+    console.table(datos)
+}
